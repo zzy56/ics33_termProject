a6907d0bbae6c1f2d3044ab90cc436dda9252558
luo144 <111543372+luo144@users.noreply.github.com>
2022-08-26 15:09:22 -0700

    pp

--- PA3_Shunwen_Luo/run.sh
+++
@@ -1,2 +0,0 @@
-python3 gui.py
-#python3 PA3_Shunwen_Luo.py -u -p 3
--- run.sh
+++
@@ -1,4 +0,0 @@
-#python3 gui.py
-python3 Final_project_Shunwen_Luo_Zhengyang_Zhuang.py
-#python3 tkinterTest.py
-#python3 threadingTest.py
